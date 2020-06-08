11e677e2540a7c68e67683909bb21ae9b75905fe
Brennien Coker <bcoker@rjf.com>
2020-06-08 15:20:10 -0400

    added cookie

--- javascript-helper.js
+++ javascript-helper.js
@@ -331,6 +331,47 @@ JSHelper.prototype.AddEvent = function(evnt, callback){
     });
 }
 /**
+ * IsEmpty
+ */
+JSHelper.prototype.IsEmpty = function(value){
+    if(value === undefined && this.params[0] !== undefined) value = this.params[0]; 
+    return (value == "" || (typeof(value) === "object" && Object.keys(value).length == 0) || value === [] || value === null || value === undefined);
+}
+/**
+ * Cookie
+ */
+function Cookie(){
+
+}
+Cookie.prototype.Create = function(name,value,days){
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+Cookie.prototype.Read = function(name){
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+Cookie.prototype.Update = function(name,value,days){
+	cookies.create(name,value,days);
+}
+Cookie.prototype.Delete = function(name){
+	cookies.create(name,"",-1);
+}
+JSHelper.prototype.Cookie = function(){
+    return new Cookie();
+}
+/**
  * Instance and optional Facade
  */
 var JSH = new JSHelper();
