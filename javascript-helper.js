571da7edd7477c944a7c42ad2ad9baad0fd2e4da
Brennien Coker <bcoker@rjf.com>
2020-06-08 12:56:03 -0400

    clean up comments

--- javascript-helper.js
+++ javascript-helper.js
@@ -175,15 +175,14 @@ JSHelper.prototype.TimeStamp =  function(date, fstr, utc) {
     utc = utc ? 'getUTC' : 'get';
     return fstr.replace (/%[YmdHMS]/g, function (m) {
       switch (m) {
-        case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
+        case '%Y': return date[utc + 'FullYear'] ();
         case '%m': m = 1 + date[utc + 'Month'] (); break;
         case '%d': m = date[utc + 'Date'] (); break;
         case '%H': m = date[utc + 'Hours'] (); break;
         case '%M': m = date[utc + 'Minutes'] (); break;
         case '%S': m = date[utc + 'Seconds'] (); break;
-        default: return m.slice (1); // unknown code, remove %
+        default: return m.slice (1); 
       }
-      // add leading zero if required
       return ('0' + m).slice (-2);
     });
 }
